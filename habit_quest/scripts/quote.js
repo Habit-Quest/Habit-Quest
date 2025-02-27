5ffa05bfdeef92671c8ec612cfae93b0a511a8fc
Jade Miller <mil19017@byui.edu>
2025-02-27 15:03:34 -0700

    random quote API

---
+++ habit_quest/scripts/quote.js
@@ -0,0 +1,11 @@
+fetch('https://api.paperquotes.com/quotes/?language=en&limit=30&offset=10&tags=love%2Clife')
+    .then(response => response.json())
+    .then(results => {
+        const quote = results.results
+        const rand = Math.floor(Math.random() * quote.length);
+        const randQuote = results.results[rand];
+
+        const p = document.createElement('p'); 
+        p.textContent = `${randQuote.quote} - ${randQuote.author}`;
+        document.body.appendChild(p);
+    })
