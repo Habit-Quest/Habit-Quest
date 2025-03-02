9d701f0546a4fd7833cb428cf42d6ce8942a9212
Jade Miller <mil19017@byui.edu>
2025-03-01 17:32:07 -0700

    j

--- habit_quest/scripts/quote.js
+++ habit_quest/scripts/quote.js
@@ -4,8 +4,10 @@ fetch('https://api.paperquotes.com/quotes/?language=en&limit=30&offset=10&tags=l
         const quote = results.results
         const rand = Math.floor(Math.random() * quote.length);
         const randQuote = results.results[rand];
+        
 
         const p = document.createElement('p'); 
         p.textContent = `${randQuote.quote} - ${randQuote.author}`;
-        document.body.appendChild(p);
+        const h1 = document.querySelector("h1");
+        h1.insertAdjacentElement("afterend", p);
     })
